1c3f49738e8128c05b22332e76606bc692f62ddc
NikolskayaAA <nikolskayaaa@mail.skblab.ru>
2025-06-13 11:34:09 +0500

    Добавила модалку

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                             <path
                                 d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
                         </svg>
-                        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
+                        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="gif"/>
                     </div>
                 </div>
             )
